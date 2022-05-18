2d78bd32b0afb63cabc03f9079c6823cc308b68e
ishvar98 <71916586+ishvar98@users.noreply.github.com>
2022-05-18 16:26:32 +0800

    Add files via upload

---
+++ filter.py
@@ -0,0 +1,37 @@
+##To list specific file format with file size in a specific directory
+
+import os, fnmatch
+
+#Replace directory path name in ""
+directory="C:/Users/Selvakumar/Downloads/"
+
+fileOfDirectory = os.listdir(directory)
+
+#Replace pattern in ""
+#ex.    = "*.zip"
+
+pattern = "*.txt"
+
+i=1
+
+fmt=pattern
+
+rfmt=fmt.replace("*.","")
+
+print("Displaying files with extension of",rfmt,"in",directory)
+for filename in fileOfDirectory:
+    if fnmatch.fnmatch(filename, pattern):
+            
+          #  print(filename)
+            os.chdir(directory)
+            size = os.stat(filename).st_size
+          #  print(size)
+            print("============================================")
+            print("||File    ||",i)
+            print("--------------------------------------------")
+            print("||Filename||",filename,"")
+            print("--------------------------------------------")
+            print("||Size    ||",size,"bytes")
+            print("============================================")
+            print("")
+            i=i+1;
